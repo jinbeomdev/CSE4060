967d1a333d60450a6f32ad4a488ceabc9aee1059
Jinbeomdev <niutn@naver.com>
2018-03-25 18:20:52 +0900

    Seted an enviroment.

---
+++ assignment_3/main.cpp
@@ -0,0 +1,22 @@
+/*
+Problem:
+Draw a simplified clock with three needles - hour, minutes, and seconds.
+The needles should rotate with different angular velocities.
+(Draw one large circle and three lines moving inside the circle.)
+
+Notes:
+1. Use GL_POINTS only. Draw multiple points.
+2. Use GLM library.
+
+Hint:
+GLM mat3 (You will learn easier way to make rotation matrices, later.)
+*/
+
+#include <glfw3.h>
+#include <vec3.hpp>
+#include <mat3x3.hpp>
+
+int main() {
+	glm::vec3 start_vec;
+	glm::mat3 rotation_mat;
+}
