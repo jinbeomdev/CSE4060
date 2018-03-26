5749635cf496c45d944f845a39c69f06b9bc9e79
Jinbeomdev <niutn@naver.com>
2018-03-27 01:00:58 +0900

    Implemented simple math class that calculates kind of vector operations

--- assignment_3/simple_math.h
+++ assignment_3/simple_math.h
@@ -4,12 +4,12 @@
 
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <mat3x3.hpp>
 
 struct SimpleMath {
 public:
-	static float Degree2Radian(float degree) {
-		return ((float)M_PI / 180.0f) * degree;
-	}
+	static float Degree2Radian(float degree);
+	static glm::mat3 GetMat3RotationMatrix(glm::vec3 point, float radian);
 };
 
 #endif // !SIMPLE_MATH_H_
